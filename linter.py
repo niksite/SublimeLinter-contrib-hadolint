99d29fde44d74bc5628c507d32ab21062fa52902
Nikolay Panov <nikolay.panov@gmail.com>
2016-02-16 12:29:43 +0300

    make flake8 happy

--- linter.py
+++ linter.py
@@ -22,7 +22,6 @@ class Hadolint(Linter):
     version_args = '-v'
     version_re = r'Haskell Dockerfile Linter v(?P<version>\d+\.\d+)'
     version_requirement = '>= 0.1'
-    # L5 DL3005 Do not use apt-get upgrade or dist-upgrade.
     regex = r'L(?P<line>\d+) (?P<message>.+)'
     multiline = False
     line_col_base = (1, 1)
@@ -34,4 +33,3 @@ class Hadolint(Linter):
     inline_settings = None
     inline_overrides = None
     comment_re = None
-
