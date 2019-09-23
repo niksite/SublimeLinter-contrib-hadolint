bc8be4f5df8881341d7849121a8d1496c15848bb
ThibF <thibaultferrante@hotmail.com>
2019-09-23 16:53:55 +0200

    Update linter.py to follow SublimeLinter evolution
    Lots of fields have been deprecated and a default value for selector is now mandatory.

--- linter.py
+++ linter.py
@@ -16,22 +16,13 @@ from SublimeLinter.lint import Linter, util
 class Hadolint(Linter):
     """Provides an interface to hadolint."""
 
-    syntax = 'dockerfile'
     cmd = 'hadolint'
-    executable = None
-    version_args = '-v'
-    version_re = r'Haskell Dockerfile Linter v(?P<version>\d+\.\d+)'
-    version_requirement = '>= 0.1'
     regex = r'(.+)\:(?P<line>[^\s]+) (?P<message>.+)'
     multiline = False
     line_col_base = (1, 1)
     tempfile_suffix = '-'
     error_stream = util.STREAM_BOTH
-    selectors = {}
     word_re = None
     defaults = {
-        '--ignore:,+': ''
+        'selector': 'text.plain'
     }
-    inline_settings = ('ignore')
-    inline_overrides = None
-    comment_re = None
